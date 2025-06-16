2c7eab61705310c6d57e43ef80819e3ddf36270b
joshuakitong <joshuakitong@gmail.com>
2025-06-17 00:31:47 +0800

    feat: add changing background based on weather condition and fixed initial loading of error message

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 import os
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = "secret_key"  # Needed for flash()
+
 API_KEY = os.getenv("API_KEY")
+
+WEATHER_BACKGROUND_MAP = {
+    "Clear": "sunny",
+    "Clouds": "cloudy",
+    "Rain": "rainy",
+    "Drizzle": "rainy",
+    "Thunderstorm": "stormy",
+    "Snow": "snowy",
+    "Mist": "foggy",
+    "Fog": "foggy",
+    "Haze": "foggy",
+    "Smoke": "foggy",
+    "Dust": "foggy",
+    "Sand": "foggy",
+    "Ash": "foggy",
+    "Squall": "stormy",
+    "Tornado": "stormy"
+}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    weather_data = None
-    error = None
-
     if request.method == "POST":
         city = request.form.get("city")
         if city:
@@ -21,15 +38,36 @@ def index():
 
             if response.status_code == 200:
                 data = response.json()
-                weather_data = {
-                    "city": data["name"],
-                    "temperature": data["main"]["temp"],
-                    "description": data["weather"][0]["description"],
-                    "icon": data["weather"][0]["icon"]
-                }
+                condition = data["weather"][0]["main"]
+
+                return redirect(url_for("index", 
+                    city_name=data["name"],
+                    temp=data["main"]["temp"],
+                    feels_like= data["main"]["feels_like"],
+                    desc=data["weather"][0]["description"],
+                    icon=data["weather"][0]["icon"],
+                    bg=WEATHER_BACKGROUND_MAP.get(condition, "default")
+                ))
             else:
-                error = "City not found. Please try again."
-    return render_template("index.html", weather=weather_data, error=error)
+                flash("City not found. Please try again.")
+                return redirect(url_for("index"))
+        else:
+            flash("Please enter a city name.")
+            return redirect(url_for("index"))
+
+    weather_data = None
+    background = request.args.get("bg", "default")
+
+    if "city_name" in request.args:
+        weather_data = {
+            "city": request.args.get("city_name"),
+            "temperature": request.args.get("temp"),
+            "feels_like": request.args.get("feels_like"),
+            "description": request.args.get("desc"),
+            "icon": request.args.get("icon")
+        }
+
+    return render_template("index.html", weather=weather_data, background=background)
 
 if __name__ == "__main__":
     app.run(debug=True)
